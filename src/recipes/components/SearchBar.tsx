cb5d942a82c6f8c07cc5307154991643c22607d0
MaximeRougieux <maximer@bam.tech>
2022-05-05 17:32:08 +0200

    Style SearchBar

--- src/recipes/components/SearchBar.tsx
+++ src/recipes/components/SearchBar.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import { TextInput } from 'react-native';
+import { StyleSheet, TextInput } from 'react-native';
 import debounce from 'lodash/debounce';
 interface SearchBarProps {
   setKeyword: (text: string) => void;
 }
 const SearchBar = ({ setKeyword }: SearchBarProps) => {
   return (
-    <TextInput onChangeText={debounce(setKeyword, 300)} testID="searchbar" />
+    <TextInput
+      onChangeText={debounce(setKeyword, 300)}
+      testID="searchbar"
+      placeholder="Search"
+      style={styles.main}
+    />
   );
 };
+
+const styles = StyleSheet.create({ main: { fontSize: 18, margin: 6 } });
 
 export default SearchBar;
